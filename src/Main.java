277f0b4faea8654ff14f4f56943ff53ddbce8a58
DenisHalil <dgh.05a4@gmail.com>
2022-06-20 05:49:16 +0300

    Second commit to the final project.

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,36 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Main {
-    public static void generatePcNumber1(){
+    public static void main(String[] args) {
         List<Integer> numbers = new ArrayList<>();
         for (int i = 1; i < 10; i++){
-                numbers.add(i);
+            numbers.add(i);
         }
         Collections.shuffle(numbers);
 
-        String noSpace = "";
+        String pcNumber1 = "";
         for (int i = 0; i < 4; i++){
-            noSpace += numbers.get(i).toString();
+            pcNumber1 += numbers.get(i).toString();
         }
-        System.out.println(noSpace);
-    }
-    public static void main(String[] args) {
-       generatePcNumber1();
+
+        //converting pcNumber1 to ArrayList
+        String[] pcNumber1Str =pcNumber1.split("");
+        List<String> pcNumber1Al = new ArrayList<String>();
+        pcNumber1Al = Arrays.asList(pcNumber1Str);
+       Scanner sc = new Scanner(System.in);
+
+       System.out.println("PLayer1, enter a four-digit number: ");
+       int number1 = sc.nextInt();
+       String number1ToString = String.valueOf(number1);
+
+       //Converting first player's number to ArrayList
+       String[] number1Str = number1ToString.split("");
+       List<String> number1Al = new ArrayList<String>();
+       number1Al = Arrays.asList(number1Str);
+
+
+
+
+
     }
 }
