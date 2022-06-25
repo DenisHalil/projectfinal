99858cdaaf1526455eed8e4960a2ac6cce48f735
DenisHalil <dgh.05a4@gmail.com>
2022-06-25 14:24:00 +0300

    Eighth commit to the final project.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,8 @@
 import java.util.*;
 
 public class Main {
-    public static void printFirstPlayerPart(List<String> pcNumber1Al, int number1, int count2){
-        String number1ToString = String.valueOf(number1);
-
-        //Converting first player's number to ArrayList
-        String[] number1Str = number1ToString.split("");
-        List<String> number1Al = new ArrayList<String>();
-        number1Al = Arrays.asList(number1Str);
-
-        //Checking if a digit of the player 1's number is in the first computer number
+    public static void printFirstPlayerPart(List<String> pcNumber1Al, List<String> number1Al, int number1){
+        //Checking if a digit of player 1's number is in the first computer number
         int count = 0;
         for (int i = 0; i < number1Al.size(); i++) {
             String digit1 = number1Al.get(i);
@@ -21,7 +14,7 @@ public class Main {
         System.out.print("Player 1, you have " + count + " cows ");
 
         //Checking positions of digits in two arrays
-        count2 = 0;
+        int count2 = 0;
         for (int i = 0; i < pcNumber1Al.size(); i++) {
             if (pcNumber1Al.get(i).equals(number1Al.get(i))) {
                 count2++;
@@ -30,17 +23,8 @@ public class Main {
         System.out.print("and " + count2 + " bulls.");
         System.out.println();
         System.out.println();
-
     }
-
-    public static void printSecondPlayerPart(List<String> pcNumber2Al, int number2, int countSecond2){
-        String number2ToString = String.valueOf(number2);
-
-        //Converting player 2's number to ArrayList
-        String[] number2Str = number2ToString.split("");
-        List<String> number2Al = new ArrayList<String>();
-        number2Al = Arrays.asList(number2Str);
-
+    public static void printSecondPlayerPart(List<String> pcNumber2Al, List<String> number2Al, int number2){
         //Checking if a digit of the player 2's number is in the second computer number
         int countSecond = 0;
         for (int i = 0; i < number2Al.size(); i++) {
@@ -53,7 +37,7 @@ public class Main {
         System.out.print("Player 2, you have " + countSecond + " cows ");
 
         //Checking positions of digits in two arrays
-        countSecond2 = 0;
+        int countSecond2 = 0;
         for (int i = 0; i < pcNumber2Al.size(); i++) {
             if (pcNumber2Al.get(i).equals(number2Al.get(i))) {
                 countSecond2++;
@@ -70,7 +54,6 @@ public class Main {
             numbers.add(i);
         }
         Collections.shuffle(numbers);
-
         String pcNumber1 = "";
         for (int i = 0; i < 4; i++) {
             pcNumber1 += numbers.get(i).toString();
@@ -82,7 +65,6 @@ public class Main {
             numbers2.add(i);
         }
         Collections.shuffle(numbers2);
-
         String pcNumber2 = "";
         for (int i = 0; i < 4; i++) {
             pcNumber2 += numbers2.get(i).toString();
@@ -90,38 +72,38 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
         while(true) {
-        //Player 1's part!
-            //Converting first computer number to ArrayList
+            //Player 1's part!
             String[] pcNumber1Str = pcNumber1.split("");
             List<String> pcNumber1Al = new ArrayList<String>();
-            pcNumber1Al = Arrays.asList(pcNumber1Str);
-
+            pcNumber1Al = Arrays.asList(pcNumber1Str);//Converting first computer number to ArrayList
 
             System.out.println("PLayer 1, enter a four-digit number: ");
             int number1 = sc.nextInt();
-            int count2 = 0;
-            printFirstPlayerPart(pcNumber1Al, number1, count2);
+            String number1ToString = String.valueOf(number1);
+            String[] number1Str = number1ToString.split("");
+            List<String> number1Al = new ArrayList<>();
+            number1Al = Arrays.asList(number1Str);//Converting first player's number to ArrayList
+            printFirstPlayerPart(pcNumber1Al, number1Al, number1);
 
             //PLayer 2's part!
-            //Converting second computer number to ArrayList
             String[] pcNumber2Str = pcNumber2.split("");
             List<String> pcNumber2Al = new ArrayList<String>();
-            pcNumber2Al = Arrays.asList(pcNumber2Str);
-
+            pcNumber2Al = Arrays.asList(pcNumber2Str); //Converting second computer number to ArrayList
             System.out.println("PLayer 2, enter a four-digit number: ");
             int number2 = sc.nextInt();
-            int countSecond2 = 0;
-            printSecondPlayerPart(pcNumber2Al, number2, countSecond2);
 
-            String number1String = String.valueOf(number1);
-            String number2String = String.valueOf(number2);
-            if (number1String.equals(pcNumber1Str) == true || number2String.equals(pcNumber2Str) == true) {
+            String number2ToString = String.valueOf(number2);
+            String[] number2Str = number2ToString.split("");
+            List<String> number2Al = new ArrayList<>();
+            number2Al = Arrays.asList(number2Str);//Converting player 2's number to ArrayList
+            printSecondPlayerPart(pcNumber2Al, number2Al, number2);
+
+            //Checking if the game is finished!
+            if (number1Al.equals(pcNumber1Al) == true || number2Al.equals(pcNumber2Al) == true){
+                System.out.println("***************************************");
                 System.out.println("Game finished!");
                 break;
             }
         }
-
-
-
     }
 }
