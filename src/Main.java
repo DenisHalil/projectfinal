b2131c3197879acde76ec487aad446eb9076e8cf
DenisHalil <dgh.05a4@gmail.com>
2022-06-27 15:34:29 +0300

    Ninth commit to the final project.

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,22 @@
 import java.util.*;
 
 public class Main {
-    public static void printFirstPlayerPart(List<String> pcNumber1Al, List<String> number1Al, int number1){
+    public static void printPlayerPart(List<String> pcNumberAl, List<String> numberAl, String player){
         //Checking if a digit of player 1's number is in the first computer number
         int count = 0;
-        for (int i = 0; i < number1Al.size(); i++) {
-            String digit1 = number1Al.get(i);
-            boolean digit1Existence = pcNumber1Al.contains(digit1);
-            if (digit1Existence == true) {
+        for (int i = 0; i < numberAl.size(); i++) {
+            String digit = numberAl.get(i);
+            boolean digitExistence = pcNumberAl.contains(digit);
+            if (digitExistence == true) {
                 count++;
             }
         }
-        System.out.print("Player 1, you have " + count + " cows ");
+        System.out.print(player + " you have " + count + " cows ");
 
         //Checking positions of digits in two arrays
         int count2 = 0;
-        for (int i = 0; i < pcNumber1Al.size(); i++) {
-            if (pcNumber1Al.get(i).equals(number1Al.get(i))) {
+        for (int i = 0; i < pcNumberAl.size(); i++) {
+            if (pcNumberAl.get(i).equals(numberAl.get(i))) {
                 count2++;
             }
         }
@@ -24,31 +24,9 @@ public class Main {
         System.out.println();
         System.out.println();
     }
-    public static void printSecondPlayerPart(List<String> pcNumber2Al, List<String> number2Al, int number2){
-        //Checking if a digit of the player 2's number is in the second computer number
-        int countSecond = 0;
-        for (int i = 0; i < number2Al.size(); i++) {
-            String digit2 = number2Al.get(i);
-            boolean digit2Existence = pcNumber2Al.contains(digit2);
-            if (digit2Existence == true) {
-                countSecond++;
-            }
-        }
-        System.out.print("Player 2, you have " + countSecond + " cows ");
-
-        //Checking positions of digits in two arrays
-        int countSecond2 = 0;
-        for (int i = 0; i < pcNumber2Al.size(); i++) {
-            if (pcNumber2Al.get(i).equals(number2Al.get(i))) {
-                countSecond2++;
-            }
-        }
-        System.out.print("and " + countSecond2 + " bulls.");
-        System.out.println();
-    }
 
     public static void main(String[] args) {
-        //Generating first computer number1
+        //Generating first computer number4
         List<Integer> numbers = new ArrayList<>();
         for (int i = 1; i < 10; i++) {
             numbers.add(i);
@@ -83,7 +61,8 @@ public class Main {
             String[] number1Str = number1ToString.split("");
             List<String> number1Al = new ArrayList<>();
             number1Al = Arrays.asList(number1Str);//Converting first player's number to ArrayList
-            printFirstPlayerPart(pcNumber1Al, number1Al, number1);
+            String playerNum1 = "Player 1,";
+            printPlayerPart(pcNumber1Al, number1Al, playerNum1);
 
             //PLayer 2's part!
             String[] pcNumber2Str = pcNumber2.split("");
@@ -96,7 +75,8 @@ public class Main {
             String[] number2Str = number2ToString.split("");
             List<String> number2Al = new ArrayList<>();
             number2Al = Arrays.asList(number2Str);//Converting player 2's number to ArrayList
-            printSecondPlayerPart(pcNumber2Al, number2Al, number2);
+            String playerNum2 = "Player 2,";
+            printPlayerPart(pcNumber2Al, number2Al, playerNum2);
 
             //Checking if the game is finished!
             if (number1Al.equals(pcNumber1Al) == true || number2Al.equals(pcNumber2Al) == true){
